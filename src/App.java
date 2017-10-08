99f2440a53dcf1133d8524964a61933686e86090
Helison <kosakmaksim@gmail.com>
2017-10-08 18:11:06 +0300

    Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
-    //Max_Kosyak
+        Calc calc = new Calc();
+        System.out.println(calc.calcPlus(4,6));
     }
 }
--- src/Calc.java
+++ src/Calc.java
@@ -1,2 +1,13 @@
 public class Calc {
+    int a;
+    int b;
+
+//    public Calc(int a,int b){
+//        this.a = a;
+//        this.b = b;
+//    }
+
+    public int calcPlus(int a ,int b){
+        return a + b;
+    }
 }
